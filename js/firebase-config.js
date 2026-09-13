cacf1d062d37d1e5485b902fa6c23da0fc3f7a41
Sreejith S <sreejithsureshkumar7@gmail.com>
2026-09-13 14:31:09 +0530

    Replace Firebase configuration with new project details

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -10,12 +10,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAHYT183gNo8IP6LPG_E0B9Pw0eILA73X0",
+  authDomain: "study-27ab0.firebaseapp.com",
+  projectId: "study-27ab0",
+  storageBucket: "study-27ab0.firebasestorage.app",
+  messagingSenderId: "915967234587",
+  appId: "1:915967234587:web:3e737ea72ce7cb4b3f1ebe",
+  measurementId: "G-PRBB2V9G00"
 };
 
 const app = initializeApp(firebaseConfig);
